35d4b9ea150ebac1dc74c3947279372d0908c793
Emile <54889613+emile2600@users.noreply.github.com>
2022-12-04 11:15:25 +0100

    Day 4

---
+++ AOC2022/Day 4/Day4.cs
@@ -0,0 +1,63 @@
+﻿using Tools.Models;
+
+namespace AOC2022.Day_4;
+
+public class Day4
+{
+    private static Input Input = new(@"Day 4/input.txt");
+    private class ElfPair
+    {
+        private IEnumerable<int> Sections1;
+        private IEnumerable<int> Sections2;
+        public ElfPair(string sections)
+        {
+            var pair = sections.Split(",");
+            Sections1 = ParseToSections(pair[0]);
+            Sections2 = ParseToSections(pair[1]);
+        }
+        private static IEnumerable<int> ParseToSections(string input)
+        {
+            var splitInput = input.Split("-");
+            var min = int.Parse(splitInput[0]); 
+            var max = int.Parse(splitInput[1]);
+            for (var i = min; i <= max; i++)
+                yield return i;
+            
+        } 
+        public bool IsFullyInContained()
+        {
+            var section2ContainsSection1 = 
+                Sections1.All(section => Sections2.Contains(section));
+            if (section2ContainsSection1)
+                return true;
+            var section1ContainsSection2 = Sections2.All(section => Sections1.Contains(section));
+            return section1ContainsSection2;
+        }
+        public bool IsOverlap()
+        {
+            var section2ContainsSection1 = 
+                Sections1.Any(section => Sections2.Contains(section));
+            if (section2ContainsSection1)
+                return true;
+            var section1ContainsSection2 = Sections2.Any(section => Sections1.Contains(section));
+            return section1ContainsSection2;
+        }
+    }
+
+    public static int AmountOfFullyContained()
+    {
+        var pairs = Input.DataSplitOnBreakLines
+            .Select(input => new ElfPair(input));
+        var sum = 
+            pairs.Sum(pair => pair.IsFullyInContained() ? 1 : 0);
+        return sum;
+    }
+    public static int AmountOfOverlaps()
+    {
+        var pairs = Input.DataSplitOnBreakLines
+            .Select(input => new ElfPair(input));
+        var sum = 
+            pairs.Sum(pair => pair.IsOverlap() ? 1 : 0);
+        return sum;
+    }
+}
--- Main/Program.cs
+++ Main/Program.cs
@@ -1,6 +1,14 @@
 ﻿using AOC_2022.Day_1;
 using AOC2022.Day_3;
+using AOC2022.Day_4;
 using AOC2022.Day2;
+
+#region Day 4
+Console.WriteLine("------------------------");
+Console.WriteLine("Day 4");
+Console.WriteLine("totalFullyContainedPairs: " + Day4.AmountOfFullyContained());
+Console.WriteLine("totalOverlaps: " + Day4.AmountOfOverlaps());
+#endregion
 
 #region Day 3
 Console.WriteLine("------------------------");
