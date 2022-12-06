6be6ba5918460a9ad04494144f747773d6ff8f64
Emile <54889613+emile2600@users.noreply.github.com>
2022-12-06 08:56:59 +0100

    Day 6

--- AOC2022/Day 6/Day6.cs
+++ AOC2022/Day 6/Day6.cs
@@ -7,30 +7,27 @@ public static class Day6
     private static readonly Input Input = new (@"Day 6\input.txt");
     public static int GetStartOfPacket(int amountOfUniqueChars)
     {
-        for (int i = 0; i < Input.Data.Length; i++)
+        for (var i = 0; i < Input.Data.Length; i++)
         {
             var groupedString = Input.Data.Substring(i, amountOfUniqueChars);
             if (AllUnique(groupedString))
                 return i + amountOfUniqueChars;
         }
-        return -1;
+        throw new Exception("No start of packet was found");
     }
     private static bool AllUnique(string input)
     {
         var array = input.ToCharArray();
         foreach (var value in array)
         {
-            var ignoreSelf = false;
+            var isDuplicate = false;
             foreach (var value2 in array)
             {
-                if (value == value2)
-                {
-                    if (ignoreSelf)
-                    {
-                        return false;
-                    }
-                    ignoreSelf = true;
-                }
+                if (value != value2)
+                    continue;
+                if (isDuplicate)
+                    return false;
+                isDuplicate = true;
             }
         }
         return true;
